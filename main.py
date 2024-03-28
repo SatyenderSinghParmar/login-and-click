9b05bf81048cb4b4d2e3ac25b4b660b77bcd0f44
SatyenderSinghParmar <103582325+SatyenderSinghParmar@users.noreply.github.com>
2024-03-28 05:41:34 +0000

    final working script part 2

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def get_driver():
 def main():
   driver = get_driver()
   element = driver.find_element(By.XPATH, "/html/body/div[1]/div/h1[1]")
+  # OR element = driver.find_element(By="xpath", value="/html/body/div[1]/div/h1[1]")
   return element.text
 
 
