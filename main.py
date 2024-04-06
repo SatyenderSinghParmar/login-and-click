5c5cf0e3f0adc6132120a902540999ffe1eebca8
SatyenderSinghParmar <103582325+SatyenderSinghParmar@users.noreply.github.com>
2024-04-06 08:33:42 +0000

    login and click done

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-import Scraping_Dynamic_Values as dy
-
+import time
+from selenium.webdriver.common.keys import Keys
 
 def get_driver():
   #set options to make browsing easier
@@ -12,19 +13,21 @@ def get_driver():
   options.add_argument("no-sandbox")
   options.add_experimental_option("excludeSwitches", ["enable-automation"])
   options.add_argument("disable-blink-features=AutomationControlled")
-
   driver = webdriver.Chrome(options=options)
-  driver.get("http://automated.pythonanywhere.com")
+  driver.get("http://automated.pythonanywhere.com/login/")
   return driver
 
 
 def main():
   driver = get_driver()
-  element = driver.find_element(By.XPATH, "/html/body/div[1]/div/h1[1]")
+  time.sleep(2)
+  driver.find_element(By.ID, "id_username").send_keys("automated")
+  time.sleep(2)
+  driver.find_element(By.ID,"id_password").send_keys("automatedautomated"+ Keys.RETURN)
+  time.sleep(2)
+  driver.find_element(By.XPATH,"/html/body/nav/div/a").click()
+  print(driver.current_url)
   # OR element = driver.find_element(By="xpath", value="/html/body/div[1]/div/h1[1]")
-  return element.text
 
 
 print(main())
-print("||")
-print(dy.main2())
